76b7a82dce8dedbf36ceb4da6c5ee0d323428b53
Ruqayyah88 <74573868+Ruqayyah88@users.noreply.github.com>
2022-04-18 04:07:23 +0100

    Add files via upload
    my cv ass   with react

---
+++ App.js
@@ -0,0 +1,82 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='container'>
+<div className='firstDiv'>
+<div className='pic'>
+
+</div>
+<div className='contact'>
+<h2>Contact</h2>
+<h4>41, Sawmill road, kukumada, Ibadan, Oyo State, Nigeria.</h4>
+<h5>Tel:+2349012143636, +2348076002552</h5>
+<h5>Email:<a href='rukayatbadmus88@gmail.com'>rukayatbadmus88@gmail.com</a></h5>
+<h5>Github:<a href='https://github.com/Ruqayyah88'> https://github.com/Ruqayyah88</a></h5>
+</div>
+<div className='education'> 
+<h2>Education</h2>
+<h4><li>Obafemi Awolowo University, Ile-Ife Osun State</li></h4>
+<h5>Bachelor of Science in Mathematics</h5>
+<h4><li>Bishop Philips Academy, Ibadan Oyo State</li></h4>
+<h5>Senior Secondary Certificate of Education</h5>
+
+</div>
+
+<div className='skills'>
+<h2>Skills</h2>
+  
+    <li>Proficient in Programming Skills like html,css and JavaScript</li>
+    <li>Willingness to Learn </li>
+    <li>Excellent Skills in Problem Solving </li>
+    <li>Have Good Interperrsonal Skill</li>
+    <li>Creativity and Resourcefulness</li>
+    <li>Efficient Time Management</li>
+  
+
+</div>
+</div>
+<div className='secDiv'>
+<div><div className='name'>
+<h2>Badmus Rukayat Temitope</h2>
+<h3>Frontend Developer</h3></div>
+<p>
+A creative frontend developer, with a graduate degree in Mathematics and a passion for producing cutting-edge yet practical designs for the web.
+<p>Team player who excels at collaborating with software experts to create a cohesive and successful design.</p>
+</p>
+</div>
+<div className='Work Experience'>
+<h2>Work Experience</h2>
+
+<h3>ICT Instructor and Class Teacher</h3>
+<h5>Al-Hijrah College, Ogun State</h5>
+<li>Planning Lessons and activities that facilitates students' acquisition of basic and advanced computer skills.</li>
+<li>Design and develop appropriate computer instructional material.</li>
+<h3>NIMC(National Identity Management Commission) Agent and Customer's Service Provider</h3>
+<h5>Kunlas Communication(Equinox Integrated Concept), Ogun State.</h5>
+<li>Using the NIMC Application Software  to enrol customers for NIN(National Identification Number).</li>
+<li>Handle customer's complaints, provides appropriate solutions and alternatives within the time limit; follow up to ensure resolution.</li>
+<li>collecting and analyzing customer's feedback</li>
+</div>
+<div className='Hobbies'>
+<h2>Hobbies</h2>
+<li>Writing Codes                 </li>
+<li>Reading and Solving Mathematical Problems</li>
+<li>Sport</li>
+
+</div>
+<div className='references'>
+<h2>Reference</h2>
+<h4>Dr B.O Olokuntoye</h4>
+<p>Obafemi Awolowo University, Ile-Ife </p>
+<p>Osun State, Nigeria </p>
+</div>
+</div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
